73e114b79ffb3904ccd99f87d0ac6b3a587260c8
Adam Wardell <awardell@ucsc.edu>
2012-05-11 16:41:28 -0700

    moved demowidget to its own file

---
+++ testwidget/static/js/demowidget.js
@@ -0,0 +1,10 @@
+var DemoWidget = function()
+{
+	var DWidget = new Widget();
+	DWidget.name = "DName";
+	DWidget.addField("Placeholder Text 1");
+	DWidget.addField("Placeholder Text 2");
+	DWidget.addField("Placeholder Text 3");
+	
+	return DWidget;
+};
