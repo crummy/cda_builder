347bf33439fc74975329b0e3ecc1904031bf7ec9
Crummy <mcrum@ucsc.edu>
2012-05-14 16:49:31 -0700

    The server works. Instructions in form.html. However, it crashes on completion.

--- testwidget/simpleserver.py
+++ testwidget/simpleserver.py
@@ -1,4 +1,4 @@
-import string, cgi, time
+import string, cgi, time, json, pickle
 from os import curdir, sep
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 #import pri
@@ -21,28 +21,23 @@ class MyHandler(BaseHTTPRequestHandler):
         #try:
         if self.headers.has_key('content-length'):
             length = int(self.headers['content-length'])
-            self.dumpReq(self.rfile.read(length))
+            self.dumpReq(self.rfile)
         else:
             self.dumpReq(None)
             
     def dumpReq(self, content):
-        response=  "<html><body>"
-        response+= "<h1>HTTP request</h1>"
-        response+= "<p>self.command= <tt>%s</tt></p>" % (self.command)
-        response+= "<p>self.path= <tt>%s</tt></p>" % (self.path)
-        response+= "<p>self.rfile= <tt>%s</tt></p>" % (content)
-        response+= "</body></html>"
-        self.sendPage("text/html", response)
+        print "content: %s" % (content)
+        jsondata = json.load(content)
+        picklefile = open('file.pkl', 'wb')
+        response = pickle.dump(jsondata, picklefile)
+        picklefile.close();
+        self.sendPage("text/html", "Success!")
     def sendPage(self, type, body):
         self.send_response(200)
         self.send_header("Content-type", type)
         self.send_header("Content-length", str(len(body)))
         self.end_headers()
         self.wfile.write(body)
-            
-        #except:
-        #    print "Failed to post: "
-        #    pass
 
 def main():
     try:
