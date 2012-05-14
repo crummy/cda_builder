4f2ad02925fef7154cb3ffe08eb602bbc5e58b09
Adam Wardell <awardell@ucsc.edu>
2012-05-14 15:36:48 -0700

    demowidget/widgetmanager separation

--- testwidget/static/js/demowidget.js
+++ testwidget/static/js/demowidget.js
@@ -8,3 +8,14 @@ var DemoWidget = function()
 	
 	return DWidget;
 };
+
+var DemoWidgetTwo = function()
+{
+	var DWidget = new Widget();
+	DWidget.name = "DName2";
+	DWidget.addField("Placeholder Text 1");
+	DWidget.addField("Placeholder Text 2");
+	DWidget.addField("Placeholder Text 3");
+	
+	return DWidget;
+};
--- testwidget/static/js/widgetmanager.js
+++ testwidget/static/js/widgetmanager.js
@@ -88,14 +88,3 @@ WidgetManager.prototype.exportFields = function()
 	}
 	return JSON.stringify(fields);
 };
-
-var DemoWidget = function()
-{
-	var DWidget = new Widget();
-	DWidget.name = "DName";
-	DWidget.addField("Placeholder Text 1");
-	DWidget.addField("Placeholder Text 2");
-	DWidget.addField("Placeholder Text 3");
-	
-	return DWidget;
-};
