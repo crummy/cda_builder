399ca299418ccb483817cbd01815b686d77f57d7
Crummy <mcrum@ucsc.edu>
2012-05-15 20:36:14 -0700

    Added basic upload functionality. Broken right now tho.

--- testwidget/simpleserver.py
+++ testwidget/simpleserver.py
@@ -22,25 +22,53 @@ class MyHandler(BaseHTTPRequestHandler):
             self.send_error(404, 'File Not Found: %s' %self.path)
             
     def do_POST(self):
-        length = int(self.headers['content-length'])
-        content = self.rfile.read(length)
-        print "post content: %s" % (content)
-        try:
-            jsondata = json.loads(content)
-            picklefile = open('clinic.pkl', 'wb')
-            response = pickle.dump(jsondata, picklefile)
-            print "saved to clinic.pkl"
-            body = "Success!"
-            picklefile.close()
-        except ValueError:
-            print "unable to parse JSON"
-            body = "Failure."
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.send_header("Content-length", str(len(body)))
-        self.end_headers()
-        self.wfile.write(body)
-        return
+        if self.path.endswith("upload"):
+            uploadPKL(self)
+        elif self.path.endswith("savepkl"):
+            saveJSON(self)
+        else:
+            body = "Unrecognized POST request"
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.send_header("Content-length", str(len(body)))
+            self.end_headers()
+            self.wfile.write(body)
+def uploadPKL( self ):
+    length = int(self.headers['content-length'])
+    content = self.rfile.read(length)
+    print "post content: %s" % (content)
+    try:
+        pickledata = pickle.loads(content)
+        jsondata = json.dump(pickledata)
+        body = jsondata
+    except ValueError:
+        body = "Failure."
+    self.send_response(200)
+    self.send_header("Content-type", "text/html")
+    self.send_header("Content-length", str(len(body)))
+    self.end_headers()
+    self.wfile.write(body)
+    return
+def saveJSON( self ):
+    length = int(self.headers['content-length'])
+    content = self.rfile.read(length)
+    print "post content: %s" % (content)
+    try:
+        jsondata = json.loads(content)
+        picklefile = open('clinic.pkl', 'wb')
+        response = pickle.dump(jsondata, picklefile)
+        print "saved to clinic.pkl"
+        body = "Success!"
+        picklefile.close()
+    except ValueError:
+        print "unable to parse JSON"
+        body = "Failure."
+    self.send_response(200)
+    self.send_header("Content-type", "text/html")
+    self.send_header("Content-length", str(len(body)))
+    self.end_headers()
+    self.wfile.write(body)
+    return
 
 def main():
     try:
